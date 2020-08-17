9615beb08b726126b495e53e06a2561f9f65ad9e
Greg Rabone <greg.rabone@gmail.com>
2020-08-17 15:40:54 +1200

    all card layouts

--- resources/views/categories/category.blade.php
+++ resources/views/categories/category.blade.php
@@ -3,27 +3,25 @@
 @section('main')
 <div class="container mt-4">
     <div class="row">
-        @foreach($articles as $article)
-        <div class="col-sm">
-            <div class="card-deck">
-                <div class="card mb-3" style="max-width: 540px;">
-                    <div class="row no-gutters">
-                        <div class="col-md-4">
-                            <img src="/storage/cover_images/{{$article->cover_image}}" class="card-img" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">{{$article->title}}</h5>
-                                <p class="card-text">{{$article->remark}}</p>
-                            </div>
-                        </div>
-                    </div>
+
+        <div class="card-deck">
+            @foreach($articles as $article)
+
+            <div class="card rounded-0 border-0">
+                <img src="/storage/cover_images/{{$article->cover_image}}" class="card-img rounded-0" alt="...">
+
+                <div class="card-body bg-dark">
+                    <h5 class="card-title text-primary">{{$article->title}}</h5>
+                    <p class="card-text text-light">{{$article->remark}}</p>
                 </div>
 
             </div>
+            @endforeach
         </div>
-        @endforeach
+
+
     </div>
 </div>
+
 
 @endsection
