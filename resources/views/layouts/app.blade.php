1d3fe79be8b96957beed8e9b5d4222ada7a3fc58
Greg Rabone <greg.rabone@gmail.com>
2020-08-22 16:56:40 +1200

    completed mailchimp signup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -105,26 +105,19 @@
                                 <a href="#" class="mx-auto pt-4">Contact</a>
                             </div>
                             <!-- Begin Mailchimp Signup Form -->
-                            <link href="//cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
-                            <style type="text/css">
-                                #mc_embed_signup {
-                                    background: #234789;
-                                    clear: left;
-                                    font: 14px Helvetica, Arial, sans-serif;
-                                }
 
-                                /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
-	                            We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-                            </style>
-                            <div id="mc_embed_signup">
-                                <form action="https://gmail.us17.list-manage.com/subscribe/post?u=c252042967ebb332a7bee3499&amp;id=57f5708cc0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                                    <div id="mc_embed_signup_scroll">
-                                        <label for="mce-EMAIL">Subscribe</label>
-                                        <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-                                        <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c252042967ebb332a7bee3499_57f5708cc0" tabindex="-1" value=""></div>
-                                        <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+
+                            <div id="mc_embed_signup" class="bg-dark rounded mt-4 p-4">
+                                <h5 class="text-uppercase text-primary text-center">Subscribe to the newsletter</h5>
+                                <form action="https://gmail.us17.list-manage.com/subscribe/post?u=c252042967ebb332a7bee3499&amp;id=57f5708cc0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" validate>
+                                    <div class="form-group pt-2">
+                                        <label for="mce-EMAIL" class="text-muted">Email</label>
+                                        <input type="email" value="" name="EMAIL" class="form-control" id="mce-EMAIL" placeholder="email address" required>
                                     </div>
+                                    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                                    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_c252042967ebb332a7bee3499_57f5708cc0" tabindex="-1" value=""></div>
+                                    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">
+
                                 </form>
                             </div>
 
